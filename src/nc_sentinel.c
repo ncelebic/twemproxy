eab6cf0483f97a1a4c6af6de18497d8f5c66b33b
Nick Celebic <nick@celebic.net>
2016-06-24 20:05:36 +0000

    Forgot the actual change

--- src/nc_sentinel.c
+++ src/nc_sentinel.c
@@ -131,8 +131,8 @@ sentinel_proc_sentinel_info(struct context *ctx, struct server *sentinel, struct
         goto error;
     }
 
-    /* get sentinel master num at line 3 */
-    msg_read_line(msg, line_buf, 3);
+    /* get sentinel master num at line 4 */
+    msg_read_line(msg, line_buf, 4);
     if (mbuf_length(line_buf) == 0) {
         log_error("read line failed from sentinel ack info when skip line not used.");
         goto error;
